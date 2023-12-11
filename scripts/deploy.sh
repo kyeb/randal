f6d10cbff7ce0913bfb3bfa4e729d2afd413e989
Kye Burchard <kyeb@mit.edu>
2023-12-10 16:43:27 -0800

    script adjustments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@ set -e
 
 cargo build --release
 
-ssh pi "sudo /bin/systemctl stop randal"
+ssh -t pi "sudo /bin/systemctl stop randal"
 
 scp target/armv7-unknown-linux-musleabihf/release/randal pi:/srv
 
-ssh pi "sudo /bin/systemctl start randal"
+ssh -t pi "sudo /bin/systemctl start randal"
---
+++ scripts/tail.sh
@@ -0,0 +1,1 @@
+ssh pi "journalctl --unit=randal --follow"
